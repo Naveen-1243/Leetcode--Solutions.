82bf2129ee0ad156d2523f350cb2d8367d0e6e9c
Gaddam Naveen <gaddamnaveen891@gmail.com>
2026-09-18 19:01:30 +0530

    Time: 546 ms (17.19%) | Memory: 32.4 MB (97.55%) - LeetSync

--- 220-contains-duplicate-iii/contains-duplicate-iii.py
+++ 220-contains-duplicate-iii/contains-duplicate-iii.py
@@ -3,17 +3,16 @@ class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
         
         s=SortedList()
+        
         for i,v in enumerate(nums):
-            if i > indexDiff:
+            if i>indexDiff:
                 s.remove(nums[i-indexDiff-1])
             
             l=v-valueDiff
             r=v+valueDiff
+
             pos=s.bisect_left(l)
-            if pos<len(s) and s[pos]<=r:
+            if pos < len(s) and s[pos]<=r:
                 return True
-        
             s.add(v)
         return False
-
-            
